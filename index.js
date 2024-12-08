e09960c779b8571cabe8a2890a08a068d9b06f7d
asifalam515 <asibulalam515@gmail.com>
2024-12-08 10:40:43 +0600

    api query applied

--- index.js
+++ index.js
@@ -55,7 +55,13 @@ async function run() {
 
     // get all rooms data form db
     app.get("/rooms", async (req, res) => {
-      const result = await roomsCollection.find().toArray();
+      const category = req.query.category;
+
+      let query = {};
+      if (category && category != "null") {
+        query = { category };
+      }
+      const result = await roomsCollection.find(query).toArray();
       res.send(result);
     });
     // get a specific room
